098e02b28fdc339124cbd6a19ba7b774b4ab4f8a
naufalr17 <mnflrizki@gmail.com>
2023-12-19 13:44:16 +0700

    Add try catch inputUrl

--- module/translate/controller.js
+++ module/translate/controller.js
@@ -1,36 +1,36 @@
-const cheerio = require('cheerio');
-const axios = require('axios');
-const fs = require('fs');
-const {Translate} = require('@google-cloud/translate').v2;
-
+const cheerio = require("cheerio");
+const axios = require("axios");
+const fs = require("fs");
+const { Translate } = require("@google-cloud/translate").v2;
 
 // ==== Inisialisasi Project ID yang sesuai dengan Credential ====
-const projectId = 'sumrizz-408115';
-const translate = new Translate({projectId});
+const projectId = "sumrizz-408115";
+const translate = new Translate({ projectId });
 
 class controller {
-  static async inputText(req,res){
-    const {text, lang} = req.body
-    
+  static async inputText(req, res) {
+    const { text, lang } = req.body;
+
     try {
       let [translation] = await translate.translate(text, `${lang}`);
-      res.status(201).json({translation: translation})
-      
+      res.status(201).json({ translation: translation });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+
+  static async inputUrl(req, res) {
+    const url = req.body.url;
+    const response = await axios.getAdapter(url);
+
+    let $ = cheerio.load(response.data);
+    let text = $("body").text();
+    try {
+      let [translation] = await translate.translate(text, `en`);
+      res.status(201).json({ translation: translation });
     } catch (error) {
       res.status(500).send(error)
     }
-
-  }
-
-  static async inputUrl(req, res){
-    const url = req.body.url
-    const response = await axios.getAdapter(url)
-
-    let $ = cheerio.load(response.data)
-    let text = $('body').text()
-
-    let [translation] = await translate.translate(text, `en`);
-    res.status(201).json({translation: translation})
   }
 
   static async sukses(req, res) {
